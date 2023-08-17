9e35d1e675b8aee1d639896221d340b40aa2ce93
Sarath P <sarath1242000@gmail.com>
2023-08-17 16:21:34 +0530

    Bug Fix

--- src/main/java/com/vedha/blog/configuration/BlogConfiguration.java
+++ src/main/java/com/vedha/blog/configuration/BlogConfiguration.java
@@ -35,45 +35,53 @@ public class BlogConfiguration implements CommandLineRunner {
     }
 
     @Override
-    public void run(String... args) throws Exception {
+    public void run(String... args) {
 
         log.info("Executing Pre-Scripts Start");
 
-        if (!roleRepository.existsByName(AppConstants.ROLE_ADMIN)) {
+        try {
 
-            RoleEntity roleAdmin = RoleEntity.builder().name(AppConstants.ROLE_ADMIN).build();
-            roleRepository.save(roleAdmin);
+            if (!roleRepository.existsByName(AppConstants.ROLE_ADMIN)) {
+
+                RoleEntity roleAdmin = RoleEntity.builder().name(AppConstants.ROLE_ADMIN).build();
+                roleRepository.save(roleAdmin);
+            }
+
+            if (!roleRepository.existsByName(AppConstants.ROLE_USER)) {
+
+                RoleEntity roleUser = RoleEntity.builder().name(AppConstants.ROLE_USER).build();
+                roleRepository.save(roleUser);
+            }
+
+            if (!userRepository.existsByName(AppConstants.ADMIN_NAME) && !userRepository.existsByEmail(AppConstants.ADMIN_EMAIL)) {
+
+                Set<RoleEntity> roleEntities = new HashSet<>();
+                RoleEntity roleAdmin = roleRepository.findByName(AppConstants.ROLE_ADMIN).orElseThrow(() -> new BlogApiException("Role Not Found : ".concat(AppConstants.ROLE_ADMIN)));
+                RoleEntity roleUser = roleRepository.findByName(AppConstants.ROLE_USER).orElseThrow(() -> new BlogApiException("Role Not Found : ".concat(AppConstants.ROLE_USER)));
+                roleEntities.add(roleAdmin);
+                roleEntities.add(roleUser);
+
+                UserEntity adminUser = UserEntity.builder().name(AppConstants.ADMIN_NAME).email(AppConstants.ADMIN_EMAIL).password(passwordEncoder.encode(AppConstants.ADMIN_PASSWORD)).roles(roleEntities).build();
+
+                userRepository.save(adminUser);
+            }
+
+            if (!userRepository.existsByName(AppConstants.USER_NAME) && !userRepository.existsByEmail(AppConstants.USER_EMAIL)) {
+
+                Set<RoleEntity> roleEntities = new HashSet<>();
+                RoleEntity roleUser = roleRepository.findByName(AppConstants.ROLE_USER).orElseThrow(() -> new BlogApiException("Role Not Found : ".concat(AppConstants.ROLE_USER)));
+                roleEntities.add(roleUser);
+
+                UserEntity normalUser = UserEntity.builder().name(AppConstants.USER_NAME).email(AppConstants.USER_EMAIL).password(passwordEncoder.encode(AppConstants.USER_PASSWORD)).roles(roleEntities).build();
+                userRepository.save(normalUser);
+            }
+
+        }catch (Exception e) {
+
+            log.error("Exception : " + e);
+        }finally {
+
+            log.info("Executing Pre-Scripts End");
         }
-
-        if (!roleRepository.existsByName(AppConstants.ROLE_USER)) {
-
-            RoleEntity roleUser = RoleEntity.builder().name(AppConstants.ROLE_USER).build();
-            roleRepository.save(roleUser);
-        }
-
-        if (!userRepository.existsByName(AppConstants.ADMIN_NAME) && !userRepository.existsByEmail(AppConstants.ADMIN_EMAIL)) {
-
-            Set<RoleEntity> roleEntities = new HashSet<>();
-            RoleEntity roleAdmin = roleRepository.findByName(AppConstants.ROLE_ADMIN).orElseThrow(() -> new BlogApiException("Role Not Found : ".concat(AppConstants.ROLE_ADMIN)));
-            RoleEntity roleUser = roleRepository.findByName(AppConstants.ROLE_USER).orElseThrow(() -> new BlogApiException("Role Not Found : ".concat(AppConstants.ROLE_USER)));
-            roleEntities.add(roleAdmin);
-            roleEntities.add(roleUser);
-
-            UserEntity adminUser = UserEntity.builder().name(AppConstants.ADMIN_NAME).email(AppConstants.ADMIN_EMAIL).password(passwordEncoder.encode(AppConstants.ADMIN_PASSWORD)).roles(roleEntities).build();
-
-            userRepository.save(adminUser);
-        }
-
-        if (!userRepository.existsByName(AppConstants.USER_NAME) && !userRepository.existsByEmail(AppConstants.USER_EMAIL)) {
-
-            Set<RoleEntity> roleEntities = new HashSet<>();
-            RoleEntity roleUser = roleRepository.findByName(AppConstants.ROLE_USER).orElseThrow(() -> new BlogApiException("Role Not Found : ".concat(AppConstants.ROLE_USER)));
-            roleEntities.add(roleUser);
-
-            UserEntity normalUser = UserEntity.builder().name(AppConstants.USER_NAME).email(AppConstants.USER_EMAIL).password(passwordEncoder.encode(AppConstants.USER_PASSWORD)).roles(roleEntities).build();
-            userRepository.save(normalUser);
-        }
-
-        log.info("Executing Pre-Scripts End");
     }
 }
--- src/main/java/com/vedha/blog/entity/RoleEntity.java
+++ src/main/java/com/vedha/blog/entity/RoleEntity.java
@@ -18,5 +18,6 @@ public class RoleEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(unique = true)
     private String name;
 }
